5d1ba54edc12cfd9978c9166acf90f567ade23e9
kandarpparikh <kandarpparikh97@gmail.com>
2020-07-26 03:02:05 +0530

    made a webpage

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+	{
+		'author': 'Kandarp Parikh',
+		'title': 'Blog Post 1',
+		'content': 'First Post Content',
+		'date_posted': 'July 26 , 2020'
+
+	},
+	{
+		'author': 'Sharad Parikh',
+		'title': 'Blog Post 2',
+		'content': 'Second Post Content',
+		'date_posted': 'July 26 , 2020'
+
+	},
+
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {'posts': posts}    
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
-
-
-# Create your views here.
+    return render(request, 'blog/about.html', {'title': 'About'})
